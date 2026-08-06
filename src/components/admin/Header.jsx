ea1f711f3fcfea31a72d0025e50329301aeb9dfe
React Dashboard <shehkhan494@gmail.com>
2026-08-06 16:06:51 +0500

    admin header

--- src/components/admin/Header.jsx
+++ src/components/admin/Header.jsx
@@ -3,22 +3,43 @@ import { HiMenuAlt2 } from "react-icons/hi";
 import { Button } from '@mui/material';
 import './header.css'
 import SearchBox from './SearchBox';
+import { MdLightMode } from "react-icons/md";
+import { FaRegBell } from 'react-icons/fa6';
 
 const Header = () => {
   return (
-
     <header className="header">
-   <div className="header-left">
-       <Button className="menu-button">
-        <HiMenuAlt2 size={24} />
-      </Button>
+      {/* Left Side: Menu, Search, aur Bell icon */}
+      <div className="header-content">
+        <Button className="menu-button">
+          <HiMenuAlt2 size={24} />
+        </Button>
 
-      <SearchBox placeholder="Search..." width="300px" />
-      </div>  
+        <SearchBox placeholder="Search..." width="300px" />
 
+     
+      </div> 
+
+      {/* Right Side: Light Mode icon */}
+      <div className="header-content"> 
+
+           <Button className="menu-button">
+          <FaRegBell size={24} />
+        </Button>
+
+        <Button className="menu-button">
+          <MdLightMode size={24} />
+        </Button>
+
+
+<div className="header-content">
+  <Button className="menu-button">
+    R
+    </Button>
+</div>
+
+      </div>
     </header>
-
-
   )
 }
 
